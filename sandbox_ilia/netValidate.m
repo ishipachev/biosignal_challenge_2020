ed73a5392faec9b47851906827da7f1ceb3fbdc4
ishipachev <ishipachev@gmail.com>
2020-05-11 01:24:32 +0200

    [ADD] Function to validate net across all validation files

--- sandbox_ilia/netValidate.m
+++ sandbox_ilia/netValidate.m
@@ -1,12 +1,12 @@
 function errMat = netValidate(params)
 
-  filerange = 21:30;
+  filerange = 21:100;
 
 %   err_rate = evalNet(sylNet, filerange, params);
 
   foldername = getPastNetwork(params);
   errMat = evalCpDir(foldername, filerange, params);
-  plotNetTrainStats(errMat, foldername);
+  plotCpsVal(errMat, foldername);
 
 end
 % fprintf("Error: %f%%\n", 100*(mean(err_rate(:,1))));
@@ -24,6 +24,7 @@ function errMat = evalCpDir(foldername, filerange, params)
     cpname = fullfile(list(ep).folder, list(ep).name);
     load(cpname, 'net');
     
+    fprintf("Validating net for %d epoch...\n", ep);
     [errRate, sylCnt, trueSylCnt] = evalNet(net, filerange, params);
     
     errMat(:, ep) = errRate;
@@ -33,39 +34,39 @@ function errMat = evalCpDir(foldername, filerange, params)
 end
 
 
-function [] = plotErrRate(errRate, sylCnt, trueSylCnt, epochNum, cpname)
-
-  figure;
-  subplot(2,1,1);
-  plot([ sylCnt, trueSylCnt ]);
-%   [~,name,~] = fileparts(cpname);
-  namestr = sprintf("Syls in files: cpname: %s, epoch %d", cpname, epochNum); 
-  title(namestr, 'Interpreter', 'none');
-  xlabel("File number");
-  ylabel("Number of syllabus");
-  legend("Detected", "True value");
-  grid on;
-
+% function [] = plotErrRate(errRate, sylCnt, trueSylCnt, epochNum, cpname)
+% 
 %   figure;
-  subplot(2,1,2);
-  plot(errRate(:,1));
-  title("Error rate across files");
-  xlabel("File number");
-  ylabel("Error rate");
-  grid on;
+%   subplot(2,1,1);
+%   plot([ sylCnt, trueSylCnt ]);
+% %   [~,name,~] = fileparts(cpname);
+%   namestr = sprintf("Syls in files: cpname: %s, epoch %d", cpname, epochNum); 
+%   title(namestr, 'Interpreter', 'none');
+%   xlabel("File number");
+%   ylabel("Number of syllabus");
+%   legend("Detected", "True value");
+%   grid on;
+% 
+% %   figure;
+%   subplot(2,1,2);
+%   plot(errRate(:,1));
+%   title("Error rate across files");
+%   xlabel("File number");
+%   ylabel("Error rate");
+%   grid on;
+% 
+% end
 
-end
-
-function [] = plotNetTrainStats(errMat, foldername)
-  figure;
-  boxplot(errMat);
-%   [~,name,~] = fileparts(foldername);
-  titilestr = sprintf("Error per epoch, folder %s", foldername);
-  title(titilestr, 'Interpreter', 'none');
-  xlabel("Epoch number");
-  ylabel("Error");
-  grid on;
-end
+% function [] = plotNetTrainStats(errMat, foldername)
+%   figure;
+%   boxplot(errMat);
+% %   [~,name,~] = fileparts(foldername);
+%   titilestr = sprintf("Error per epoch, folder %s", foldername);
+%   title(titilestr, 'Interpreter', 'none');
+%   xlabel("Epoch number");
+%   ylabel("Error");
+%   grid on;
+% end
 
 
 function foldername = getPastNetwork(params)
