885b1e6a7941a1c6f9ab1d0b795c4d34173cd6b4
ishipachev <ishipachev@gmail.com>
2020-05-14 14:13:40 +0200

    [MOD] Cleanup

--- sandbox_eva/rms.m
+++
@@ -1,3 +0,0 @@
-function rms_dB = RMS(segment,fs);
-out = sqrt(sum(segment.^2)./length(segment));
-rms_dB = 20*log10(out);
--- sandbox_eva/zcr.m
+++
@@ -1,5 +0,0 @@
-% Zero-cross rate
-% https://www.asee.org/documents/zones/zone1/2008/student/ASEE12008_0044_paper.pdf
-function out=ZCR(segment,fs);
-zcr = sum(abs(diff(segment>0)));             
-out = zcr*fs/(2*length(segment));
