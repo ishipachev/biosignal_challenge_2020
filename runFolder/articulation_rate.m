eee5e9a3e7e5ec31125ef74b7630cb21c69295d2
ishipachev <ishipachev@gmail.com>
2020-05-27 13:48:19 +0200

    [FIX]Fixed in according to updated structure and some functions

--- runFolder/articulation_rate.m
+++ runFolder/articulation_rate.m
@@ -1,9 +1,13 @@
 function [syl, dur] = articulation_rate(sig, fs)
+%% Solution relies on 44100 frequency sample rate
 
+commonFolder = '../common';
 sylsFolder = '../detect_syllabuses';
 speechFolder = '../detect_speech';
 optFname = 'trainOpt.mat';
 wcNetFname = 'net_checkpoint*.mat';
+
+addpath(commonFolder);
 
 %% Syllabus
 addpath(sylsFolder);
@@ -15,14 +19,13 @@ netNameStruct = dir(fullfile(sylsFolder, bestnetFolder, wcNetFname));
 netFname = fullfile(netNameStruct.folder, netNameStruct.name);
 load(netFname, 'net');
 
-s = soundNormalize(sig);
+s = soundNormalize(sig, fs);
 f = extract(params.afe, s);
 f = featNormalize(f);
 
 sylsCnt = getScore(net, f, params);
 
 rmpath(sylsFolder);
-
 %% Speech duration
 addpath(speechFolder);
 
@@ -34,7 +37,7 @@ netNameStruct = dir(fullfile(speechFolder, bestnetFolder, wcNetFname));
 netFname = fullfile(netNameStruct.folder, netNameStruct.name);
 load(netFname, 'net');
 
-s = soundNormalize(sig);
+s = soundNormalize(sig, fs);
 f = extract(params.afe, s);
 f = featNormalize(f);
 
@@ -45,5 +48,7 @@ rmpath(speechFolder);
 
 syl = sylsCnt;
 dur = speechDur;
+
+rmpath(commonFolder);
 
 end
